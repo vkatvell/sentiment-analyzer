966d2541a8ab211c077838a57ab027204414c6e7
vkatvell <venkatvellankiphoto@gmail.com>
2023-06-29 18:34:32 -0500

    Created word map with words and scores

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
+use std::collections::HashMap;
 use std::error::Error;
 use std::time::Instant;
-
 #[derive(Debug, Deserialize)]
 struct Tweet {
     sentiment: u8,
@@ -12,29 +12,39 @@ struct Tweet {
     tweet: String,
 }
 
-fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+fn read_from_file(path: &str, mut wordmap: HashMap<String, f32>) -> Result<(), Box<dyn Error>> {
+    // Reading from csv path
     let mut reader = csv::Reader::from_path(path)?;
 
-    let headers = reader.headers();
-    println!("Headers: {:?}", headers);
-
+    // Count records
     let mut count: i32 = 0;
 
+    // Iterating through records and deserializing into Tweet struct
     for result in reader.deserialize() {
         let record: Tweet = result?;
 
-        // Discarding values
-        let _ = { record.query };
-        let _ = { record.date };
-        let _ = { record.user };
+        // Tokenizing tweets
+        let collection: Vec<&str> = record.tweet.split_whitespace().collect();
 
-        println!(
-            "Sentiment: {}, Tweet {}: {}",
-            record.sentiment, record.id, record.tweet
-        );
+        // Scoring words higher if pos. sentiment
+        for word in &collection {
+            if record.sentiment == 4 {
+                *wordmap.entry(word.to_string()).or_insert(0.0) += 5.0;
+            }
+            *wordmap.entry(word.to_string()).or_insert(0.0) -= 1.0;
+        }
+
+        // Discarding unused struct values
+        let _ = record.query;
+        let _ = record.date;
+        let _ = record.user;
+        let _ = record.id;
+        let _ = record.sentiment;
 
         count += 1;
     }
+
+    println!("\n\nWords and their score: {:?}", wordmap);
 
     println!("\nRead {} records", count);
 
@@ -42,8 +52,11 @@ fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
+    // Creating hashmap for words and their score
+    let word_map: HashMap<String, f32> = HashMap::new();
+
     let start_time = Instant::now();
-    if let Err(e) = read_from_file("./sent_analysis_data/train_dataset_20k.csv") {
+    if let Err(e) = read_from_file("./sent_analysis_data/train_dataset_10val.csv", word_map) {
         eprintln!("{}", e);
     }
     let elapsed_time = start_time.elapsed();
